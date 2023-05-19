1573cb10a823fb9a927f009a048ba8792dc92f03
Fabio Berchtold <jamesclonk@jamesclonk.ch>
2023-05-19 18:16:39 +0200

    Update init_minecraft.sh

--- init_minecraft.sh
+++ init_minecraft.sh
@@ -5,13 +5,13 @@ apt-get -y update
 apt-get -y install unzip zlibc openssl zip curl wget ca-certificates netcat
 
 # install minio client CLI
-wget 'https://dl.minio.io/client/mc/release/linux-amd64/mc' \
+wget 'https://dl.minio.io/client/mc/release/linux-amd64/mc'
 mv mc /usr/local/bin/mc
 chmod 755 /usr/local/bin/mc
 
 # install rcon CLI
-wget 'https://github.com/itzg/rcon-cli/releases/download/1.4.8/rcon-cli_1.4.8_linux_amd64.tar.gz' \
-tar -C /usr/local/bin -xzf rcon-cli_1.4.8_linux_amd64.tar.gz \
+wget 'https://github.com/itzg/rcon-cli/releases/download/1.4.8/rcon-cli_1.4.8_linux_amd64.tar.gz'
+tar -C /usr/local/bin -xzf rcon-cli_1.4.8_linux_amd64.tar.gz
 rm -f rcon-cli_1.4.8_linux_amd64.tar.gz
 chmod 755 /usr/local/bin/rcon-cli
 
